1e2c8702ed931539e43d5bccd7100d326be25229
Antonio Burgello <burgello.antonio@gmail.com>
2024-11-16 02:43:36 +0000

    Stuff

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -3,6 +3,7 @@ class Champion < ApplicationRecord
   has_many :abilities, dependent: :destroy
   has_many :champion_types
   has_many :types, through: :champion_types
+  belongs_to :region
 
   validates :name, :title, presence: true
 end
