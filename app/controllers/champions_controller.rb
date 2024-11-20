5104d180994286d18a046d9cfc4b5b06556b7bb0
Antonio Burgello <burgello.antonio@gmail.com>
2024-11-20 01:17:24 +0000

    Stuff

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -2,7 +2,12 @@ class ChampionsController < ApplicationController
   before_action :set_champion, only: [ :show ]
 
   def index
+    @regions = Region.all
     @champions = Champion.order(:name)
+
+    if params[:region_id].present?
+      @champions = @champions.where(region_id: params[:region_id])
+    end
   end
 
 
