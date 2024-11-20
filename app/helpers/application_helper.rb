b12b7b925d8a4d50f0b1de6f5e07576b4d5da9ed
Antonio Burgello <burgello.antonio@gmail.com>
2024-11-20 00:31:03 +0000

    Stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 module ApplicationHelper
-
+  def skin_gradient_style(rarity)
+    case rarity.downcase
+    when "mythic"
+      "background: linear-gradient(to bottom, #7943ED, #5E2B99);"
+    when "ultimate"
+      "background: linear-gradient(to bottom, #FFA500, #CC8400);"
+    when "legendary"
+      "background: linear-gradient(to bottom, #FF3232, #B22222);"
+    when "epic"
+      "background: linear-gradient(to bottom, #00C8FF, #007BB5);"
+    when "transcendent"
+      "background: linear-gradient(to bottom, #A2A2D0, #6E6E96);"
+    when "common"
+      "background: linear-gradient(to bottom, #808080, #585858);"
+    else
+      "background: linear-gradient(to bottom, #0A1428, #091428);" # Default
+    end
+  end
 end
